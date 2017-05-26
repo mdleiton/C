cf4724b49d0785bf481b7b757b2616c48adbcfe2
mdleiton <mau_leiton96@hotmail.com>
2017-05-26 02:23:25 -0500

    parte 1 sin conexion de letra z a la letra A

--- taller.c
+++ taller.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <string.h>
+#define MAXIMO 26
+
+char * encriptar(char nuv_fra[MAXIMO],int num);
+
 int main(){
-	printf("taller3");
-	return 0;
+    int llave;
+    char cadena[MAXIMO];
+
+    printf("\t\t\t\t\tCIFRADO CICLICO:\n");
+    printf("Introducir mensaje: ");
+    fgets(cadena,100,stdin);
+
+    //while(getchar()!='\n');
+    printf("ingrese la llave numerica: ");
+    scanf("%d",&llave);
+    //while(getchar()!='\n');
+    printf("\nLa frase encriptado es:\n%s\n", encriptar(cadena, llave));
+    return 0;
 }
+
+char *  encriptar(char cadena[MAXIMO],int llave){
+    int i=0;
+    while(cadena[i]!='\0'){
+        cadena[i]=cadena[i]+llave;
+        i++;
+    }
+    return cadena;  
+}
+
 
