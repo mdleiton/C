302c2906cf056a73d3d6240a3a21ba2e19dd72af
mdleiton <mau_leiton96@hotmail.com>
2017-05-26 02:28:19 -0500

    valido ciclo en letras mayusculas

--- taller.c
+++ taller.c
@@ -1,32 +1,35 @@
 #include <stdio.h>
 #include <string.h>
-#define MAXIMO 26
+#define MAXIMO 30
 
 char * encriptar(char nuv_fra[MAXIMO],int num);
 
-int main(){
+int  main(){
     int llave;
     char cadena[MAXIMO];
 
     printf("\t\t\t\t\tCIFRADO CICLICO:\n");
     printf("Introducir mensaje: ");
     fgets(cadena,100,stdin);
-
-    //while(getchar()!='\n');
     printf("ingrese la llave numerica: ");
     scanf("%d",&llave);
-    //while(getchar()!='\n');
     printf("\nLa frase encriptado es:\n%s\n", encriptar(cadena, llave));
     return 0;
 }
 
 char *  encriptar(char cadena[MAXIMO],int llave){
     int i=0;
+    int dif;
     while(cadena[i]!='\0'){
-        cadena[i]=cadena[i]+llave;
-        i++;
+	if(cadena[i]>='A' && cadena[i]<='Z'){
+		if((dif=cadena[i]+llave-'Z')>0){
+			cadena[i]='A'+dif-1;
+		}else{
+        		cadena[i]=cadena[i]+llave;
+        	}
+	}
+	i++;
     }
     return cadena;  
 }
-
 
