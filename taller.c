601e8cd28b6b4a5ff2345174585466bfaf614cca
mdleiton <mau_leiton96@hotmail.com>
2017-05-26 02:31:04 -0500

    PARTE 1 version final completa

--- taller.c
+++ taller.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-#define MAXIMO 30
+#include <stdlib.h>
+#define MAXIMO 120
 
 char * encriptar(char nuv_fra[MAXIMO],int num);
 
@@ -20,23 +21,44 @@ int  main(){
 char *  encriptar(char cadena[MAXIMO],int llave){
     int i=0;
     int dif;
- 	llave=llave % 26;
-    while(cadena[i]!='\0'){
-	if(cadena[i]>='A' && cadena[i]<='Z'){
-		if((dif=cadena[i]+llave-'Z')>0){
-			cadena[i]='A'+dif-1;
-		}else{
-        	cadena[i]+=llave;
-        }
-	}else if(cadena[i]>='a' && cadena[i]<='z'){
-		if((dif=cadena[i]+llave-'z')>0){
-			cadena[i]='a'+dif-1;
-		}else{
-        	cadena[i]+=llave;
-        }
-	}
-	i++;
+    /* si la llave es negativa y mayor a 26 */
+    if(llave < 0){
+		llave=(-1*llave)%26;
+		llave=(-1*llave);
+		while(cadena[i]!='\0'){
+			if(cadena[i]>='A' && cadena[i]<='Z'){
+				if((dif=cadena[i]+llave-'A')<=0){
+					cadena[i]='Z'+dif;
+				}else{
+					cadena[i]+=llave;
+				}
+			}else if(cadena[i]>='a' && cadena[i]<='z'){
+				if((dif=cadena[i]+llave-'a')<=0){
+					cadena[i]='z'+dif;
+				}else{
+					cadena[i]+=llave;
+				}
+			}
+			i++;
+		}
+    }else{
+	   	llave=llave % 26;
+		while(cadena[i]!='\0'){
+			if(cadena[i]>='A' && cadena[i]<='Z'){
+				if((dif=cadena[i]+llave-'Z')>0){
+					cadena[i]='A'+dif-1;
+				}else{
+					cadena[i]+=llave;
+				}
+			}else if(cadena[i]>='a' && cadena[i]<='z'){
+				if((dif=cadena[i]+llave-'z')>0){
+					cadena[i]='a'+dif-1;
+				}else{
+					cadena[i]+=llave;
+				}
+			}
+			i++;
+		}
     }
     return cadena;  
 }
-
