0123e52b36be70c132f9c2493a161ef696eb9fe1
mdleiton <mau_leiton96@hotmail.com>
2017-05-27 02:10:03 -0500

    union de la funcion morse en la rama parte2 a la rama master

--- taller.c
+++ taller.c
@@ -5,23 +5,27 @@
 
 char * encriptar(char nuv_fra[MAXIMO],int num);
 char * morse(char cadena[MAXIMO]);
+int validarllave(char llave[MAXIMO]);
 
 int  main(int argc, char *argv[] ){
     int llave;
+    char llavec[MAXIMO];
     char cadena[MAXIMO];
     printf("\t\t\t\t\tCIFRADO CICLICO:\n");
     if(argc <= 1){
 	    printf("Introducir mensaje: ");
 	    fgets(cadena,100,stdin);
 	    printf("ingrese la llave numerica: ");
-	    scanf("%d",&llave);
-	    printf("\nLa cadena encriptado es:\n%s\n", encriptar(cadena, llave));
-	    printf("\nMensaje cifrado en morse:\n%s\n", morse( cadena));
+	    scanf("%s",llavec);
+        llave= validarllave(llavec);
+        printf("\nLa cadena encriptado es:\n%s\n", encriptar(cadena, llave));
+        printf("\nMensaje cifrado en morse:\n%s\n", morse( cadena));
 	}else if (argc<=2){
 	    printf("\nle falto un argumento \n la cantidad correcta de argumentos: 2 \n llave  n cadena \n");
 	}else if (argc<=3){
-	    printf("\nMensaje cifrado:\n%s\n", encriptar( argv[2], atoi(argv[1])));
-	    printf("\nMensaje cifrado en morse:\n%s\n", morse( argv[2]));
+	    llave= validarllave(argv[1]);
+        printf("\nMensaje cifrado:\n%s\n", encriptar( argv[2], llave));
+        printf("\nMensaje cifrado en morse:\n%s\n", morse( argv[2]));
 	}else{
 		printf("no ingrese la cantidad correcta de argumentos: \n llave  \n cadena \n");
 	}
@@ -156,3 +160,21 @@ char * morse(char cadena[MAXIMO]){
 
 
 }
+
+int validarllave(char llave[MAXIMO]){
+    int llaveNum=0;
+    while(llaveNum == 0)
+    {
+        llaveNum = atoi(llave);
+        if(llaveNum == 0){
+            printf("\n\nLlave numérica incorrecta!");
+            printf("\nIngrese la llave numérica: ");
+            scanf("%s", llave);
+        }
+    }
+
+    return llaveNum;
+}
+
+
+
