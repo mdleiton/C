a76bd929e595a476f3cfda23ede9d5441fcfddd4
mdleiton <mau_leiton96@hotmail.com>
2017-05-29 03:05:51 -0500

    agregando al modo interactivo la solicitud de tipo de cifrado

--- taller.c
+++ taller.c
@@ -1,39 +1,79 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "./src/cifrado.c"
 #include "./src/codificacion.c"
-#define MAXIMO 1200
+#define MAXIMO 1024
 
 int validarllave(char llave[MAXIMO]);
+char validartipo(char tipo[]);
 
 int  main(int argc, char *argv[] ){
-    int llave;
+    int llave ;
     char llavec[MAXIMO];
     char cadena[MAXIMO];
-    printf("\t\t\t\t\tCIFRADO CICLICO:\n");
+    memset(cadena,0,MAXIMO);
+    char tipo[10];
+    char tipoValido;
     if(argc <= 1){
-	    printf("Introducir mensaje: ");
-	    scanf("%s",cadena);
-	    printf("ingrese la llave numerica: ");
-	    scanf("%s",llavec);
-        llave= validarllave(llavec);
-        printf("\nLa cadena encriptado es:\n%s\n", cifradoCiclico(cadena, llave));
-        printf("\nMensaje cifrado en morse:\n%s\n", claveMorse( cadena));
-	}else if (argc<=2){
-	    printf("\nle falto un argumento \n la cantidad correcta de argumentos: 2 \n llave  n cadena \n");
+    	printf("TIPO DE CIFRADO: ");
+	    scanf("%s",tipo);
+	    tipoValido=validartipo(tipo);
+	    switch(tipoValido){
+	    	case 'C':
+	    		printf("\t\t\t\t\tCIFRADO CICLICO:\n");
+	    		printf("ingrese la llave numerica: ");
+			    scanf("%s",llavec);
+		        llave= validarllave(llavec);
+		        memset(llavec,0,MAXIMO);
+
+			    printf("Introducir mensaje: ");
+			    fgets(cadena,MAXIMO,stdin);
+		        printf("\nLa cadena encriptado es:\n%s\n", cifradoCiclico(cadena, llave));
+		        printf("\nMensaje cifrado en morse:\n%s\n", claveMorse( cadena));
+	    		break;
+	    	case 'A':
+	    		printf("\nAUTOLLAVE\n");
+	    		break;
+	    	case 'P':
+	    		printf("con contrasena");
+	    		break;
+	    }
 	}else if (argc<=3){
+	    printf("\nle falto un argumento \n la cantidad correcta de argumentos: 3 \n llave  \n cadena  \n tipo de cicfrado  \n");
+	}else if (argc<=4){
+		tipoValido=validartipo(argv[3]);
 	    llave= validarllave(argv[1]);
         printf("\nMensaje cifrado:\n%s\n", cifradoCiclico( argv[2], llave));
         printf("\nMensaje cifrado en morse:\n%s\n", claveMorse( argv[2]));
 	}else{
-		printf("no ingrese la cantidad correcta de argumentos: \n llave  \n cadena \n");
+		printf("no ingreso la cantidad correcta de argumentos: 3 \n llave  \n cadena  \n tipo de cicfrado \n");
 	}
 	return 0;
 }
 
+char validartipo(char tipo[]){
+    int bandera=0;
+    while(bandera == 0)
+    {
+        if(strlen(tipo)>1 ){
+            printf("\n\nDebe ingresar solo una letra!");
+            printf("\nIngrese el tipo de cifrado correctamente: ");
+            scanf("%s", tipo);
+            tipo[0]=tolower(tipo[0]);
+        }else if ( tipo[0]!='C' && tipo[0] != 'A' && tipo[0] != 'P'){
+        	printf("\n\nla letra ingresada es incorrecta!");
+            printf("\nIngrese el tipo de cifrado correctamente: ");
+            scanf("%s", tipo);
+        }else{
+        	bandera=1;
+        }
+    }
+    return tipo[0];
 
 
+}
 int validarllave(char llave[MAXIMO]){
     int llaveNum=0;
     while(llaveNum == 0)
@@ -45,7 +85,6 @@ int validarllave(char llave[MAXIMO]){
             scanf("%s", llave);
         }
     }
-
     return llaveNum;
 }
 
