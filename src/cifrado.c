e6ba2b58cf3037020416f6f6d837dc7c72adbda1
whugo43 <whugo43@hotmail.com>
2017-05-29 21:19:18 -0500

    funciones cifradoAutollave modificado

--- src/cifrado.c
+++ src/cifrado.c
@@ -1,4 +1,7 @@
 #include "../include/cifrado.h"
+#define MAX_ROWS 26
+#define MAX_COLS 26
+#define MAX 26
 
 char* cifradoCiclico(char* mensaje, int llave){
 	int i=0;
@@ -47,76 +50,44 @@ char* cifradoCiclico(char* mensaje, int llave){
 //////////////////////////////////////////////////
 char* cifradoAutollave(char* mensaje, char* llave)
 {
-//GENERAR TABULA RECTA
-    char tabula_recta[MAX_ROWS][MAX_COLS];
-    int pos , row , col;
-     
-    for( row=0; row < MAX_ROWS; row++){
-        pos = 0;
-        for( col=0; col < MAX_COLS; col++){
-            if('A'+row+col > 'Z' && row > 0){
-                tabula_recta[row][col] = 'A'+ pos;
-                pos++;
-            }
-        else if('A' + row + col == 'Z')
-            tabula_recta[row][col] = 'Z';
-        else
-            tabula_recta[row][col] = 'A'+row+col;
-        }
-    }
-     
-// GENERAR NUEVA CONTRASEÑA A COMPARAR
-int l=0;
-char nuevcont[MAX];
+//printf("%s\n",mensaje);
+char abecedario[MAX]="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char nuevoabc[MAX]={0};
+char codigo[MAX]={0};
 int i=0;
-while(contrasena[i]!='\0'){ 
-        if (mensaje[i]==32){
-	nuevcont[i]=mensaje[i];}
-	else{     
-	nuevcont[i]=contrasena[l];l++;
-	}
+//printf("\n%s",abecedario);
+
+while(llave[i]!='\0'){     
+	nuevoabc[i]=llave[i];
 	i++;
 }
-nuevcont[i]=contrasena[i-1];
-
 int j=0;
-int k= i+1;
-for (k;mensaje[k]!='\0';k++){
- if(mensaje[j]==32){j++;}
-	nuevcont[k]=mensaje[j];
-j++;
+for (int c=i;c<26;c++){
+	nuevoabc[c]=abecedario[j];
+	j++;
 }
+//printf("\n%s",nuevoabc);
 
-
-//GENERAR EL CODIGO CODIFICADO
- i=0;
-int tempfil, tempcol=0;
-char codigo[MAX]={0};
-for (i;mensaje[i]!='\0';i++){
-	if (mensaje[i]==32){
-		codigo[i]=mensaje[i];i++;}
-
-	for(col=0; col<MAX_COLS; col++){
-		if(tabula_recta[col][0]==mensaje[i]){
-			tempfil=col;
-		}
-		if(tabula_recta[0][col]==nuevcont[i]){
-			tempcol=col;
-		}	
-	}
-	codigo[i]=tabula_recta[tempfil][tempcol];
+int h=0;
+while(mensaje[h]!='\0'){
+for (int l=0;abecedario[l]!='\0';l++){
+if (mensaje[h]==abecedario[l])
+{
+//printf("\n%d\n abecedario: ",l);
+//printf("%c abecedario: ",abecedario[l]);
+//printf("%c abecedario: ",nuevoabc[l]);
+codigo[h]=nuevoabc[l];
+}else if (mensaje[h] == 32){
+codigo[h]=mensaje[h];
 }
-
-
-
-//printf("\n%s\n",mensaje);
-//printf("\n%s\n",nuevcont);
-
-//printf("\n%s\n",codigo)
-printf("Mensaje encriptado:      ");
+}
+h++;
+}
+//printf("\n%s\n",codigo);
 mensaje=codigo;
+
+printf("Mensaje encriptado:      ");
 return mensaje;
-//return codigo;
  
 }
 
@@ -138,15 +109,15 @@ while(llave[i]!='\0'){
 	i++;
 }
 int j=0;
-for (i;i<26;i++){
-	nuevoabc[i]=abecedario[j];
+for(int d=i;d<26;d++){
+	nuevoabc[d]=abecedario[j];
 	j++;
 }
 //printf("\n%s",nuevoabc);
 
 int h=0;
 while(mensaje[h]!='\0'){
-for (int l=0;abecedario[l]!='\0';l++){
+for(int l=0;abecedario[l]!='\0';l++){
 if (mensaje[h]==abecedario[l])
 {
 //printf("\n%d\n abecedario: ",l);
