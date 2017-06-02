a103e0b6551b83744ea3aed0bc6860ef60bf4e47
mdleiton <mau_leiton96@hotmail.com>
2017-06-01 21:35:44 -0500

    taller3-reparado despues del merge equivocado

--- include/cifrado.h
+++
@@ -1,3 +0,0 @@
-char* cifradoCiclico(char* mensaje, int llave);
-char* cifradoAutollave(char* mensaje, char* llave);
-char* cifradoContrasena(char* mensaje, char* llave );
--- include/codificacion.h
+++
@@ -1,2 +0,0 @@
-char* claveMorse(char* mensaje);
-
